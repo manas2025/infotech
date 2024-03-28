31345d2fb8f3ac16dbe6c583458d7df75a2c3532
manas2025 <106134826+manas2025@users.noreply.github.com>
2024-03-28 23:19:15 +0530

    Update Port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = 3000;
+const port = 3000 || process.env.PORT;
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 const path = require('path');
